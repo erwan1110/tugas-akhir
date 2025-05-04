fb0d02943c3094e3754e005c931c3e3022d78941
erwan1110 <bokuton6@gmail.com>
2025-05-04 15:59:47 +0700

    update sore 04 Mei 2025

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+         const splide = new Splide( '.splide', {
+           type   : 'loop',
+           drag   : 'free',
+           focus  : 'center',
+           perPage: 3,
+           autoScroll: {
+             speed: 1,
+           },
+         } );
+         splide.mount();
+
 var data = [
     'Agya',
     'Calya',
@@ -121,4 +132,74 @@ const user = {
     age: '25'
 }
 let dataUser=''
-for(let data in user) {}
+for(let data in user) {
+dataUser += user[data] + ' '
+}
+console.log(dataUser)
+
+
+const users =  [
+    {
+        "id": 7,
+        "email": "michael.lawson@reqres.in",
+        "first_name": "Michael",
+         "last_name": "Lawson",
+         "avatar": "https://reqres.in/img/faces/7-image.jpg"
+     },
+     {
+        "id": 8,
+        "email": "lindsay.ferguson@reqres.in",
+        "first_name": "Lindsay",
+        "last_name": "Ferguson",
+        "avatar": "https://reqres.in/img/faces/8-image.jpg"
+    },
+    {
+        "id": 9,
+        "email": "tobias.funke@reqres.in",
+        "first_name": "Tobias",
+        "last_name": "Funke",
+        "avatar": "https://reqres.in/img/faces/9-image.jpg"
+    },
+    {
+        "id": 10,
+         "email": "byron.fields@reqres.in",
+         "first_name": "Byron",
+         "last_name": "Fields",
+         "avatar": "https://reqres.in/img/faces/10-image.jpg"
+     },
+     {
+         "id": 11,
+         "email": "george.edwards@reqres.in",
+         "first_name": "George",
+         "last_name": "Edwards",
+         "avatar": "https://reqres.in/img/faces/11-image.jpg"
+     },
+     {
+        "id": 12,
+         "email": "rachel.howell@reqres.in",
+         "first_name": "Rachel",
+         "last_name": "Howell",
+         "avatar": "https://reqres.in/img/faces/12-image.jpg"
+     }
+ ]
+ 
+ const listUser = document.getElementById('userDataPrint')
+ users.forEach(user => {
+     for(let v in user){
+         console.log(user[v])
+         //document.writeln(user[v] + '<br>')
+         //document.getElementById("userDataPrint").innerHTML = user[v];
+         const dataUserMap = users.map(user => 
+            `<div class="col-lg-4 col-md-6">
+            <img src="${user.avatar}" width="100%"><br>
+            ${user.email}<br>
+            ${user.first_name}<br>
+            ${user.last_name}
+            </div>`
+            ).join('')
+            
+            listUser.innerHTML = `<div class="row">${dataUserMap}</div>`   
+            }
+         })
+         
+         
